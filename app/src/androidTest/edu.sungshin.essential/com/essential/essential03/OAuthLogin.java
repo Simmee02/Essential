87409e4b25a54de484fbf75db3dfaf3b761b03f4
Simmee02 <114573447+Simmee02@users.noreply.github.com>
2024-03-12 01:14:39 +0900

    백업

---
+++ app/src/androidTest/edu.sungshin.essential/com/essential/essential03/OAuthLogin.java
@@ -0,0 +1,4 @@
+package com.essential.essential03;
+
+public class OAuthLogin {
+}
---
+++ app/src/main/java/com/essential/essential03/UserFragment.java
@@ -0,0 +1,52 @@
+package com.essential.essential03;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.essential03.R;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class UserFragment extends Fragment {
+
+    TextView logout;
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_user, container, false);
+
+        logout = v.findViewById(R.id.logout);
+        mAuth = FirebaseAuth.getInstance();
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),LoginActivity.class);
+                startActivity(intent);
+                Toast.makeText(getActivity().getApplicationContext(), "로그아웃 되셨습니다.", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        return v;
+    }
+
+}
